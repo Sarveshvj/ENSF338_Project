739f9306391b8f73b3ba6b5426aade449cef0446
KJ597 <Jaswalkuljot@gmail.com>
2026-03-31 15:10:22 -0600

    Added comments for clear code.

--- service_queue.py
+++ service_queue.py
@@ -1,8 +1,15 @@
+
 class Request:
+    """Represents the service that is request from the system.
+    Will be used in our heap class as request objects
+    
+    """
+    
     def __init__(self, id, name, room, priority, description=None ):
         self.id = id
         self.name = name
         self.room = room
         self.description = description
         self.priority = priority
+
 

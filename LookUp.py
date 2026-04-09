3f7bbe145ec3109d6b5aa10eb62a728468926b78
Steven <steven.wu@ucalgary.ca>
2026-04-08 20:30:43 -0600

    update lookup system and connect with graph

--- LookUp.py
+++ LookUp.py
@@ -1,16 +1,3 @@
-
-
-
-
-
-# Important:
-# - get_building() and get_room() return None if not found
-# - remove_building() will also remove all rooms under that building
-
-
-
-
-
 class Room:
     def __init__(self, room_id, capacity, room_type):
         self.room_id = room_id
@@ -27,7 +14,7 @@ class Building:
         self.building_id = building_id
         self.name = name
         self.location = location
-        self.rooms = {}   # room_id -> Room
+        self.rooms = {}
 
     def add_room(self, room):
         self.rooms[room.room_id] = room
@@ -35,7 +22,8 @@ class Building:
     def get_room(self, room_id):
         if room_id in self.rooms:
             return self.rooms[room_id]
-        return None
+        else:
+            return None
 
     def remove_room(self, room_id):
         if room_id in self.rooms:
@@ -46,111 +34,157 @@ class Building:
 
 
 class LookupSystem:
-    def __init__(self):
-        self.buildings = {}  
-        self.rooms = {}       
+    def __init__(self, campus=None):
+        self.buildings = {}
+        self.rooms = {}
+        self.campus = campus
 
-    #building
     def add_building(self, building):
         self.buildings[building.building_id] = building
+
+        if self.campus != None:
+            alreadyThere = False
+            for x in self.campus.buildings:
+                if x.building_id == building.building_id:
+                    alreadyThere = True
+            if alreadyThere == False:
+                self.campus.addBuilding(building.building_id)
 
     def get_building(self, building_id):
         if building_id in self.buildings:
             return self.buildings[building_id]
+
+        if self.campus != None:
+            for x in self.campus.buildings:
+                if x.building_id == building_id:
+                    return x
+
         return None
 
     def remove_building(self, building_id):
         if building_id in self.buildings:
-            building = self.buildings[building_id]
+            b = self.buildings[building_id]
 
-            # remove all rooms under this building
-            for room_id in building.rooms:
-                if room_id in self.rooms:
-                    del self.rooms[room_id]
+            for r in list(b.rooms.keys()):
+                if r in self.rooms:
+                    del self.rooms[r]
 
             del self.buildings[building_id]
 
-    #room 
+        if self.campus != None:
+            target = None
+            for x in self.campus.buildings:
+                if x.building_id == building_id:
+                    target = x
+                    break
+
+            if target != None:
+                for r in list(target.rooms.keys()):
+                    if r in self.rooms:
+                        del self.rooms[r]
+                del self.campus.buildings[target]
+
     def add_room(self, building_id, room):
-        if building_id in self.buildings:
-            self.buildings[building_id].add_room(room)
-            self.rooms[room.room_id] = room
-        else:
+        b = self.get_building(building_id)
+
+        if b == None:
             print("building not found")
+            return
+
+        b.add_room(room)
+        self.rooms[room.room_id] = room
 
     def get_room(self, room_id):
         if room_id in self.rooms:
             return self.rooms[room_id]
+
+        for b in self.buildings:
+            if room_id in self.buildings[b].rooms:
+                return self.buildings[b].rooms[room_id]
+
+        if self.campus != None:
+            for x in self.campus.buildings:
+                if room_id in x.rooms:
+                    return x.rooms[room_id]
+
         return None
 
     def remove_room(self, room_id):
         if room_id in self.rooms:
             del self.rooms[room_id]
 
-        # also remove from building
-        for building in self.buildings.values():
-            if room_id in building.rooms:
-                building.remove_room(room_id)
+        removed = False
+
+        for b in self.buildings:
+            if room_id in self.buildings[b].rooms:
+                self.buildings[b].remove_room(room_id)
+                removed = True
                 break
 
-    #demo print
+        if removed == False and self.campus != None:
+            for x in self.campus.buildings:
+                if room_id in x.rooms:
+                    del x.rooms[room_id]
+                    break
+
     def print_all(self):
-        print("Buildings:", list(self.buildings.keys()))
-        print("Rooms:", list(self.rooms.keys()))
+        print("Buildings:")
+        for b in self.buildings:
+            print(b)
+
+        print("Rooms:")
+        for r in self.rooms:
+            print(r)
 
 
 def main():
-    import time   
-    system = LookupSystem()
+    from campusmap_pathnav import Campus
+    import time
 
-    # add buildings
+    campus = Campus()
+    lookup = LookupSystem(campus)
+
     b1 = Building("ICT", "ICT Building", (0, 0))
     b2 = Building("ENG", "Engineering", (1, 1))
 
-    system.add_building(b1)
-    system.add_building(b2)
+    lookup.add_building(b1)
+    lookup.add_building(b2)
 
-    # add rooms
-    r1 = Room("ICT-121", 100, "lecture")
-    r2 = Room("ENG-201", 50, "lab")
+    room1 = Room("ICT-121", 100, "lecture")
+    room2 = Room("ENG-201", 50, "lab")
 
-    system.add_room("ICT", r1)
-    system.add_room("ENG", r2)
+    lookup.add_room("ICT", room1)
+    lookup.add_room("ENG", room2)
 
-    # lookup existing
-    print("Find ICT:", system.get_building("ICT"))
-    print("Find ICT-121:", system.get_room("ICT-121"))
+    print("Building ICT:", lookup.get_building("ICT"))
+    print("Room ICT-121:", lookup.get_room("ICT-121"))
 
-    # lookup missing
-    print("Find SCI:", system.get_building("SCI"))
-    print("Find XXX:", system.get_room("XXX"))
+    print("Missing building:", lookup.get_building("SCI"))
+    print("Missing room:", lookup.get_room("XXX"))
 
-    # remove
-    system.remove_room("ICT-121")
-    print("After remove ICT-121:", system.get_room("ICT-121"))
+    lookup.remove_room("ICT-121")
+    print("After removing ICT-121:", lookup.get_room("ICT-121"))
 
-    system.remove_building("ENG")
-    print("After remove ENG:", system.get_building("ENG"))
+    lookup.remove_building("ENG")
+    print("After removing ENG:", lookup.get_building("ENG"))
 
-    system.print_all()
+    lookup.print_all()
 
-    print("\n--- Performance Test ---")
+    t1 = time.time()
+    lookup.get_building("ICT")
+    t2 = time.time()
+    print("small lookup time:", t2 - t1)
 
-    start = time.time()
-    system.get_building("ICT")
-    end = time.time()
-    print("Lookup time (small data):", end - start)
+    i = 0
+    while i < 1000:
+        temp = Building("B" + str(i), "Building" + str(i), (i, i))
+        lookup.add_building(temp)
+        i += 1
 
-    for i in range(10000):
-        b = Building("B" + str(i), "Building" + str(i), (i, i))
-        system.add_building(b)
-
-    start = time.time()
-    system.get_building("B9999")
-    end = time.time()
-    print("Lookup time (large data):", end - start)
-
-    print("Lookup time is similar -> close to O(1)")
+    t3 = time.time()
+    lookup.get_building("B999")
+    t4 = time.time()
+    print("large lookup time:", t4 - t3)
 
 
 if __name__ == "__main__":
